1847df712567ee60d094263794f7bf66a132ad6e
CDAMXI <cdachongm@gmail.com>
2025-03-09 13:51:55 +0100

    Fix method call for retrieving current thread name in ExThread class

--- Tema1_Act3/ExThread.java
+++ Tema1_Act3/ExThread.java
@@ -1,14 +1,12 @@
 package Tema1_Act3;
 
-import java.util.concurrent.*;
-
 public class ExThread {
     public static void main(String[] args) {
-        System.out.println(Thread.currentThread.getName());
+        System.out.println(Thread.currentThread().getName());
         for(int i = 0; i < 10; i++){
             Thread t = new Thread("MyThread-" + i) {
                 public void run() {
-                    System.out.println("Excecuted by " + Thread.currentThread.getName());
+                    System.out.println("Excecuted by " + Thread.currentThread().getName());
                 }
             };
             t.start();
